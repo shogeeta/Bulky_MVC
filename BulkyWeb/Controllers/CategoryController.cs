eb07754a60d08ee8763f9f0a8ee92480f48ec434
Geetika <geetikakgec2009@gmail.com>
2025-07-06 17:04:00 +0530

    Add category creation with validation and feedback
    Updated the `Create` action in `CategoryController` to handle POST requests for creating new categories, including model state validation and redirection upon success. Enhanced the `Category` model with data annotations for `Name` and `DisplayOrder` to enforce validation rules. Modified the `Create.cshtml` view to display validation messages for user input errors.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -20,5 +20,19 @@ namespace BulkyWeb.Controllers
         public IActionResult Create() {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                //ModelState.IsValid checks if the model is valid based on data annotations
+                //if not valid, it will return to the same view with validation errors
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                //return RedirectToAction("Index", "Category"); applies when it has to go to diff controller action method
+                return RedirectToAction("Index");
+            }    
+            return View();
+        }
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -10,9 +10,11 @@ namespace BulkyWeb.Models
         [Required]
         //used for client side data annotations to showfeild values as text tagHelpers
         [DisplayName("Category Name")]
+        [MaxLength(30, ErrorMessage = "Name cannot be longer than 50 characters.")]
         public string Name { get; set; }
         //used for client side data annotations to showfeild values as text tagHelpers
         [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100.")]
         public int DisplayOrder { get; set; }    
     }
 }
