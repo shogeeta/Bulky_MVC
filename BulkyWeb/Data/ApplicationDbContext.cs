a9c35e01993fd707ca95aeb51c3302f5075cac4f
Geetika <geetikakgec2009@gmail.com>
2025-07-06 16:37:47 +0530

    #added few things in project for entiry framework and .net core leanring
    Add category management and update styling framework
    
    - Updated `BulkyWeb.csproj` to include Entity Framework Core packages for SQL Server (v8.0.17).
    - Added `CategoryController` for managing categories with listing and creation methods.
    - Created `ApplicationDbContext` for database context and seeded initial category data.
    - Added migration files for creating and seeding the `Categories` table.
    - Introduced `Category` model class with properties for `Id`, `Name`, and `DisplayOrder`.
    - Modified `Program.cs` to configure SQL Server connection for `ApplicationDbContext`.
    - Created `Create.cshtml` and `Index.cshtml` views for category management.
    - Updated `_Layout.cshtml` for improved navigation and footer styling with Bootstrap icons.
    - Modified `appsettings.json` to include the database connection string.
    - Updated `bootstrap.css` to version 5.3.7 with the "lux" theme and improved typography.

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+               
+        }
+        //create tables in entity framework
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //seed data
+            modelBuilder.Entity<Category>().HasData(
+                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
